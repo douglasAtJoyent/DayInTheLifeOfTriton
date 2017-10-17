5ed064dd7d726a66c3b7dc1f9df9d6182c994a1f
douglasAtJoyent <douglas.andeson@joyent.com>
2017-10-17 16:39:09 -0500

    Update to runJMeter.sh

--- runJMeter.sh
+++ runJMeter.sh
@@ -1,3 +1,3 @@
 #/usr/bin/bash
 
-/apache-jmeter-3.3/bin/jmeter -Dhost=${HOST} -DdockerHost=${DOCKER_URL} -Dcert=${DOCKER_CERT_PATH} -Dscriptlocation=/DayInTheLifeOfTriton/gen_key.sh -Dkeyfile=${SSH_FILE} -Daccount=${SDC_ACCOUNT} -Dusername=${SDC_USERNAME} -DdockerPath=`which docker` -Dduration=${DURATION} -Dtls=${TLS_FLAG} -n -t /DayInTheLifeOfTriton/JMeter/DayInTheLifeUnified.jmx -o ${OUTPUT_DIRECTORY} 
+/apache-jmeter-3.3/bin/jmeter -Dhost=${HOST} -DdockerHost=${DOCKER_URL} -Dcert=${DOCKER_CERT_PATH} -Dscriptlocation=/DayInTheLifeOfTriton/gen_key.sh -Dkeyfile=${SSH_FILE} -Daccount=${SDC_ACCOUNT} -Dusername=${SDC_USERNAME} -DdockerPath=`which docker` -Dduration=${DURATION} -DThreads=${THREADS} -Dtls=${TLS_FLAG} -n -t /DayInTheLifeOfTriton/JMeter/DayInTheLifeUnified.jmx -o ${OUTPUT_DIRECTORY} 
